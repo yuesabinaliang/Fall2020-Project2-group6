d4969342e8739acc5e4cd39e150e05bb3a894c8e
Yue Liang <sabina_ly@163.com>
2021-02-03 01:26:14 -0500

    update

--- app/server.R
+++ app/server.R
@@ -63,7 +63,7 @@ shinyServer(function(input,output){
                 return new L.DivIcon({ html: '<div><span>' + sum + '</span></div>', className: 'marker-cluster' + c, iconSize: L.point(size, size)});
               }")
     
-    na_drop <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/data-by-modzcta.csv")
+    na_drop <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/data-by-modzcta.csv")
     na_drop <- melt(na_drop, id.vars=1:3, value.name="Count", variable.name="Category")
     zip_code <- read.csv('https://raw.githubusercontent.com/daling11/xxxxxxxx/master/zip_code_database.csv')
     colnames(zip_code)<-c('MODIFIED_ZCTA', 'lat', 'lon')
@@ -104,21 +104,21 @@ shinyServer(function(input,output){
     # tab panel 3 - Home 
     
     ## Yue Liang boxes part starts---------------------------------------------------------------------------------------
-    case_number <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/summary.csv")
+    case_number <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/summary.csv")
     output$total_case <- renderText({
-        formatC(as.numeric(as.character(case_number[1,2])), format="d", big.mark=",") 
+        formatC(as.numeric(as.character(case_number[3,2])), format="d", big.mark=",") 
     })
     
     output$hospital_rate <- renderText({
-        paste(round(100*as.numeric(as.character(case_number[2,2]))/as.numeric(as.character(case_number[1,2])), 1), "%", sep="")
+        paste(round(100*as.numeric(as.character(case_number[4,2]))/as.numeric(as.character(case_number[3,2])), 1), "%", sep="")
     })
     
     output$death_rate <- renderText({
-        paste(round(100*as.numeric(as.character(case_number[3,2]))/as.numeric(as.character(case_number[1,2])), 1), "%", sep="")
+        paste(round(100*as.numeric(as.character(case_number[5,2]))/as.numeric(as.character(case_number[3,2])), 1), "%", sep="")
     })
     
     output$date <- renderText({
-        paste(substring(as.character(case_number[5,2]),1,3),substring(as.character(case_number[5,2]),str_locate(as.character(case_number[5,2]), ' ')[1,1]+1,str_locate(as.character(case_number[5,2]), ',')[1,1]-1),sep=" ")
+        paste(substring(as.character(case_number[8,2]),1,3),substring(as.character(case_number[8,2]),str_locate(as.character(case_number[8,2]), ' ')[1,1]+1,str_locate(as.character(case_number[8,2]), ',')[1,1]-1),sep=" ")
     })
     ## Yue Liang boxes part ends---------------------------------------------------------------------------------------
     
@@ -126,7 +126,7 @@ shinyServer(function(input,output){
     
     ## Yue Liang Statistical Plots starts -----------------------------------------------------------------------------
     
-    by_boro <- data.frame(read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-boro.csv"))
+    by_boro <- data.frame(read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/by-boro.csv"))
     by_boro$CASE_RATE <- by_boro$CASE_RATE/100000
     by_boro$HOSPITALIZED_RATE <- by_boro$HOSPITALIZED_RATE/100000
     by_boro$DEATH_RATE <- by_boro$DEATH_RATE/100000
@@ -167,7 +167,7 @@ shinyServer(function(input,output){
         xlab("BOROUGH_GROUP")
     })
 
-    by_poverty <- data.frame(read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-poverty.csv"))
+    by_poverty <- data.frame(read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/by-poverty.csv"))
     
     by_poverty$CASE_RATE_ADJ <- by_poverty$CASE_RATE_ADJ/100000
     by_poverty$HOSPITALIZED_RATE_ADJ <- by_poverty$HOSPITALIZED_RATE_ADJ/100000
@@ -218,12 +218,12 @@ shinyServer(function(input,output){
         poverty
     })
         
-    index <- data.frame(read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/case-hosp-death.csv"))
+    index <- data.frame(read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/trends/data-by-day.csv"))
     
-    index_df <- melt(index[,1:4],id="DATE_OF_INTEREST")
-    index_df$DATE_OF_INTEREST<- as.Date(index_df$DATE_OF_INTEREST,format = "%m/%d/%y")
+    index_df <- melt(index[,1:4],id="date_of_interest")
+    index_df$date_of_interest<- as.Date(index_df$date_of_interest,format = "%m/%d/%y")
     
-    index_gg <- ggplot(index_df,aes(x=DATE_OF_INTEREST,y=value,color=variable,group= variable)) +
+    index_gg <- ggplot(index_df,aes(x=date_of_interest,y=value,color=variable,group= variable)) +
                         geom_line() +
                         scale_x_date(date_labels = "%b/%d")
     
